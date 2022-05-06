fcde04bf34e9f83eb097f99d107c7329ed3b8642
Eduardo Oviedo <eduardo.oviedo@gmail.com>
2022-05-05 21:19:45 -0600

    auth ready

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+import './config/firebase';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import RootNavigation from './navigation';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <RootNavigation />
       <StatusBar style="auto" />
     </View>
   );
